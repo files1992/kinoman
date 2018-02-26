0e79e24d60ea9a4df1750de9c502e598c5d4ff4d
Bartosz Worobiec <worobiecbartosz@gmail.com>
2018-02-26 23:05:51 +0100

    Deserializer

---
+++ Kinoman/Kinoman/Services/Impl/Deserializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Kinoman.Services.Impl
+{
+    public class Deserializer : IDeserializer
+    {
+        public T Deserialize<T>(string data)
+        {
+            var deserializedObject = JsonConvert.DeserializeObject<T>(data);
+            return deserializedObject;
+        }
+    }
+}
