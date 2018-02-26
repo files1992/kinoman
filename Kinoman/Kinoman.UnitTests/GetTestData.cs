ed54297c5df998196b8cb95b4860a494c195df36
Bartosz Worobiec <worobiecbartosz@gmail.com>
2018-02-26 22:36:33 +0100

    Add GetData class

---
+++ Kinoman/Kinoman.UnitTests/GetTestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Kinoman.UnitTests.Enums;
+
+namespace Kinoman.UnitTests
+{
+    public class GetTestData
+    {
+        public Dictionary<FileType,string> Path { get; } = new Dictionary<FileType, string>()
+        {
+            {FileType.MultiKino,"TestData/MultiKino.json"}
+        };
+
+        public string GetSampleResponse()
+        {
+            return null;
+        }
+    }
+}
