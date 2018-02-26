dae850432af9b28b6ed2ecd2d5589f3d2dbf4271
Bartosz Worobiec <worobiecbartosz@gmail.com>
2018-02-26 22:13:33 +0100

    First few red tests

---
+++ Kinoman/Kinoman.UnitTests/GetTestDataTests.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Kinoman.UnitTests
+{
+    public class GetTestDataTests
+    {
+        public GetTestData Sut { get; set; }
+        [TestFixtureSetUp]
+        public void TfSetUp()
+        {
+            Sut = new GetTestData();
+        }
+
+        
+        [Test]
+        public void ReadFileTest()
+        {
+            var Sut = new FileDataProvider();
+
+            var contents = Sut.GetSampleResponse();
+
+            Assert.That(contents, Is.Not.Null);
+        }
+
+        [Test]
+        public void FileIsExist()
+        {
+            foreach (var path in Sut.Path.Values)
+            {
+                var fileIsThere = File.Exists(path);
+                Assert.That(fileIsThere, $"File missing: {path}");
+            }
+        }
+    }
+}
