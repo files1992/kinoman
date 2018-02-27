45b46eb8c06de17c1db79f1d02430310ea5641b3
Bartosz Worobiec <worobiecbartosz@gmail.com>
2018-02-27 18:19:46 +0100

    Data Provider Service testes

---
+++ Kinoman/Kinoman.UnitTests/DataProviderServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Kinoman.Entities.MultiKino;
+using Kinoman.Services;
+using Kinoman.Services.Impl;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Kinoman.UnitTests
+{
+    public class DataProviderServiceTests
+    {
+        private IDownloadService _downloadServiceMock;
+        private IDeserializer _deserializerMock;
+        private DataProviderService _sut;
+
+
+        [TestFixtureSetUp]
+        public void TfSetUp()
+        {
+            _deserializerMock = Substitute.For<IDeserializer>();
+            _downloadServiceMock = Substitute.For<IDownloadService>();
+            _downloadServiceMock.DownloadData(Arg.Any<string>()).Returns("file content");
+            _sut = new DataProviderService(_downloadServiceMock,_deserializerMock);
+        }
+
+        [Test]
+        public void CheckThatDownloadDataMethodIsCalled()
+        {
+            var result = _sut.GetCurrentData<MultiKinoShowing>();
+            _downloadServiceMock.Received(1).DownloadData(Arg.Any<string>());
+        }
+
+    }
+}
